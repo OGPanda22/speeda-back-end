9c7f89ce000f952f25fda268ffbf4f28eca5f21f
zlibres22 <zacklibres@gmail.com>
2025-07-10 21:47:53 -0400

    edited code

--- server.js
+++ server.js
@@ -15,18 +15,19 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin (like Postman)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: function(origin, callback){
+//     // allow requests with no origin (like Postman)
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){
+//       var msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+//       return callback(new Error(msg), false);
+//     }
+//     return callback(null, true);
+//   },
+//   credentials: true
+// }));
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
